330d1db49f09f431e2258e64cb613658264a3b84
DeathHound <acraig176@hotmail.com>
2026-07-15 14:53:02 +0100

    Add `Telescope fd` alias keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,3 +2,7 @@
 -- uses <leader> is defined, so keep this at the top of the config.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+
+vim.keymap.set("n", "fd", ":Telescope fd<CR>", { desc = "`fd` opens Telescope find files" })
+vim.keymap.set("n", "ff", ":Telescope fd<CR>", { desc = "`ff` opens Telescope find files" })
