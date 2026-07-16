9711111791fa615324e4f08714d2eb555883a1d2
DeathHound <acraig176@hotmail.com>
2026-07-16 13:51:27 +0100

    Add jump to symbol definition keymap alias

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,7 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "fd", ":Telescope fd<CR>", { desc = "`fd` opens Telescope find_files" })
 vim.keymap.set("n", "ff", ":Telescope fd<CR>", { desc = "`ff` opens Telescope find_files" })
 vim.keymap.set("n", "gl", ":Telescope live_grep<CR>", { desc = "`gl` openes Telescope live_grep" })
+vim.keymap.set("n", "gd", "<C-]>", { desc = "`gd` jumps to the highlighted symbol's definition" })
 
 -- Visual Mode
 vim.keymap.set("x", "p", [["_dP]], { desc = "Lower case 'p' pastes a yank and preserves it in clipboard" })
