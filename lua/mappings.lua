726ecbdf64896671b2afe6f1ee3de967b9d5bafc
DeathHound <acraig176@hotmail.com>
2026-07-29 11:07:04 +0100

    feat: more keymaps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,6 +9,12 @@ vim.keymap.set("n", "ff", ":Telescope fd<CR>", { desc = "`ff` opens Telescope fi
 vim.keymap.set("n", "gl", ":Telescope live_grep<CR>", { desc = "`gl` openes Telescope live_grep" })
 vim.keymap.set("n", "gd", "<C-]>", { desc = "`gd` jumps to the highlighted symbol's definition" })
 vim.keymap.set("n", "gwd", "<C-w>]", { desc = "`gwd` jumps to the highlighted symbol's definition in a new buffer" })
+vim.keymap.set("n", "gwvd", function()
+    vim.cmd([[vsplit]])
+    vim.lsp.buf.definition()
+end, { desc = "`gwvd` jumps to the highlighted symbol's definition in a new buffer (vsplit)" })
+vim.keymap.set({'n', 'v'}, 'd', '"_d', { desc = "Delete without copying" })
+vim.keymap.set({'n', 'v'}, 'D', '"_D', { desc = "Delete line end without copying" })
 
 -- Visual Mode
 vim.keymap.set("x", "p", [["_dP]], { desc = "Lower case 'p' pastes a yank and preserves it in clipboard" })
